edee8c1971cf95e17818b7380942750fe868a21d
arashk <yadegar.arashk@gmail.com>
2025-04-15 09:20:18 -0700

    feat: module updated

--- src/time-links/time-links.module.ts
+++ src/time-links/time-links.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TimeLinksService } from './time-links.service';
 import { TimeLinksController } from './time-links.controller';
+import { RandomWordsModule } from 'src/random-words/random-words.module';
+import { TimeLinkEntity } from './entities/time-link.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RandomWordsService } from 'src/random-words/random-words.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TimeLinkEntity]), RandomWordsModule],
   controllers: [TimeLinksController],
-  providers: [TimeLinksService],
+  providers: [TimeLinksService,RandomWordsService],
 })
 export class TimeLinksModule {}
